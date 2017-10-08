8f7664cae12e7c6441d784671a7e1973273b1df6
rguo123 <richardg7890@gmail.com>
2017-10-08 01:50:35 -0400

    pickle bookmark serve file done

--- source/bvarjavand/boss_parse/boss_parse.py
+++ source/bvarjavand/boss_parse/boss_parse.py
@@ -14,8 +14,8 @@ def get_collman_em_cube(token,z_range,x_range,y_range):
     #jsoncube = cube.tolist() #to load, use np.array(json.loads(cube)['data'])
     #with open('cube.pickle', 'wb') as f:
     #    pickled = pickle.dump(cube, f)
-    metadata = {'id':'01_00_00','dim':[z_range,x_range,y_range],'annotaters':[]}
-    metacube = {'metadata':metadata, 'data':cube}
+    metadata = {'id':'01_00_00','dim':[z_range,x_range,y_range],'annotaters':[]} #TODO: id not helpful?
+    metacube = {'metadata':metadata, 'data':cube} #TODO: make a class
     with open('metacube.pickle', 'wb') as f:
         metapickled = pickle.dump(metacube, f)
 
---
+++ source/bvarjavand/boss_parse/boss_serve.py
@@ -0,0 +1,37 @@
+import pickle
+import scipy.misc
+from PIL import Image
+
+#TODO: eventually will get mongodb to keep track of what slice
+
+def get_bookmark(bookmark_file):
+    bookmark = pickle.load(open(bookmark_file, "rb"))
+    return bookmark
+
+def save_bookmark(bookmark, bookmark_file):
+    pickle.dump(bookmark, open(bookmark_file, "wb"))
+
+def load_slice(filename, bookmark_file): #expects pickle file
+    try:
+        data = pickle.load(open(filename, "rb"))
+        bookmark = get_bookmark(bookmark_file)
+
+        metadata = data["metadata"]
+        cube = data["data"]
+        serve = cube[bookmark]
+        img_name = str(bookmark).join(metadata["id"].split("_")[1:])  + ".jpg"
+
+        im = Image.fromarray(serve)
+        im.save(img_name)
+        bookmark += 1
+        save_bookmark(bookmark, bookmark_file)
+    except:
+        raise("oops forgot to load file prob")
+
+def test_load():
+    save_bookmark(0, "bookmark.pickle")
+    load_slice("metacube.pickle", "bookmark.pickle")
+
+
+if __name__ == '__main__':
+    test_load()
